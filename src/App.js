24854ddb2cccdd9bd9a75c63533e4e0b398407ba
AlanGtz59 <alanminjarez59@gmail.com>
2024-03-19 11:48:41 -0600

    se le agrego el formulario para el inicio de sesion del empleado, viva el CSS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import Start from './components/start'
 export default function App() {
   return (
     <h1 className="text-3xl font-bold underline">
-      Hello world!
+      <Start/>
     </h1>
   )
 }
--- src/components/start.jsx
+++ src/components/start.jsx
@@ -1,5 +1,49 @@
-export default function Hello(){
-    return(<div>
-        Hello
-    </div>)
-}
+import React, { useState } from 'react';
+import './App.css';
+
+const LoginForm = () => {
+  const [employeeId, setEmployeeId] = useState('');
+  const [role, setRole] = useState('');
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aquí puedes manejar la lógica de inicio de sesión
+    console.log('Empleado ID:', employeeId);
+    console.log('Rol:', role);
+  };
+
+  return (
+    <>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <form  onSubmit={handleSubmit}>
+        <h2>Ingresa tus datos</h2>
+
+        <label>
+        <p>Rol del empleado: </p>
+          <select  value={role} onChange={(e) => setRole(e.target.value)} required> {"\n"}
+            <option value="">Seleccionar rol</option>
+            <option value="Mesero">Mesero</option>
+            <option value="Cocinero">Cocinero</option>
+            <option value="Cajero">Cajero</option>
+          </select>
+        </label>
+
+        <label>
+        <p style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>Numero de empleado: </p>
+          <input 
+            style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin:'10px' }}
+            type="text" 
+            value={employeeId} 
+            onChange={(e) => setEmployeeId(e.target.value)} 
+            required
+          />
+        </label>
+
+        <button type="submit">Iniciar sesión</button>
+      </form></div>
+    </>
+  );
+};
+
+export default LoginForm;
