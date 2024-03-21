4d1bf842822a37238a8f76ac2b5af203c5f311d6
AlanGtz59 <alanminjarez59@gmail.com>
2024-03-21 10:45:28 -0600

    wireframe de imagen para boton categorias

--- src/components/boton_imagenes.jsx
+++ src/components/boton_imagenes.jsx
@@ -1,114 +1,17 @@
-import React from 'react';
 import './form.css'
+import imagen from './img/gato_pizza.jpg'
 
-function bot_img(){
-    return(
-      <div>
-  <div style={{ flex: '1' }}>
-  <button
-      className="button1"
-      style={{
-        padding: '40px',
-        backgroundImage: `url(${require('./img/7d7416b4820df975d753ff31da0b9909.jpg')})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center'
-      }}
-    >
-      Click aqui
-    </button>
-    <button
-      className="button1"
-      style={{
-        padding: '40px',
-        backgroundImage: `url(${require('./img/7d7416b4820df975d753ff31da0b9909.jpg')})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center'
-      }}
-    >
-      Click aqui
-    </button>
-    <button
-      className="button1"
-      style={{
-        padding: '40px',
-        backgroundImage: `url(${require('./img/7d7416b4820df975d753ff31da0b9909.jpg')})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center'
-      }}
-    >
-      Click aqui
-    </button>
+function bot_img() {
+  return (
+    <div>
+      <div className="div-boton">
+      <img src={imagen} height="200px" width="200 px" />
+      <button className="my-button">
+          Categoria
+      </button>
     </div>
-  <div style={{ flex: '2' }}>
-  <button
-      className="button1"
-      style={{
-        padding: '40px',
-        backgroundImage: `url(${require('./img/7d7416b4820df975d753ff31da0b9909.jpg')})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center'
-      }}
-    >
-      Click aqui
-    </button>
-    <button
-      className="button1"
-      style={{
-        padding: '40px',
-        backgroundImage: `url(${require('./img/7d7416b4820df975d753ff31da0b9909.jpg')})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center'
-      }}
-    >
-      Click aqui
-    </button>
-    <button
-      className="button1"
-      style={{
-        padding: '40px',
-        backgroundImage: `url(${require('./img/7d7416b4820df975d753ff31da0b9909.jpg')})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center'
-      }}
-    >
-      Click aqui
-    </button></div>
-  <div style={{ flex: '1' }}>
-  <button
-      className="button1"
-      style={{
-        padding: '40px',
-        backgroundImage: `url(${require('./img/7d7416b4820df975d753ff31da0b9909.jpg')})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center'
-      }}
-    >
-      Click aqui
-    </button>
-    <button
-      className="button1"
-      style={{
-        padding: '40px',
-        backgroundImage: `url(${require('./img/7d7416b4820df975d753ff31da0b9909.jpg')})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center'
-      }}
-    >
-      Click aqui
-    </button>
-    <button
-      className="button1"
-      style={{
-        padding: '40px',
-        backgroundImage: `url(${require('./img/7d7416b4820df975d753ff31da0b9909.jpg')})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center'
-      }}
-    >
-      Click aqui
-    </button>
   </div>
-</div>
+
 
     ) 
 }
