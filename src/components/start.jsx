c4eca12201221ba117890cf1be768446c0f1fd76
AlanGtz59 <a1120150064@utch.edu.mx>
2024-03-20 18:12:42 -0600

    Se mejoro un poco mas la visualizacion del login y se agregaron varias cosas en App.css

--- src/components/start.jsx
+++ src/components/start.jsx
@@ -1,49 +1,28 @@
-import React, { useState } from 'react';
-import './App.css';
+import React from 'react';
+import './App.css'; // Importa el archivo CSS
 
-const LoginForm = () => {
-  const [employeeId, setEmployeeId] = useState('');
-  const [role, setRole] = useState('');
-
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Aquí puedes manejar la lógica de inicio de sesión
-    console.log('Empleado ID:', employeeId);
-    console.log('Rol:', role);
-  };
-
+function Login() {
   return (
-    <>
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-        <form  onSubmit={handleSubmit}>
-        <h2>Ingresa tus datos</h2>
-
+    <div className="login-container">
+      <form className="formulario">
+        <h2 className="h1" style={{padding: '20px'}}>Ingresa tus datos</h2>
         <label>
-        <p>Rol del empleado: </p>
-          <select  value={role} onChange={(e) => setRole(e.target.value)} required> {"\n"}
+          <p>Rol del empleado:</p>
+          <select>
             <option value="">Seleccionar rol</option>
             <option value="Mesero">Mesero</option>
             <option value="Cocinero">Cocinero</option>
             <option value="Cajero">Cajero</option>
           </select>
         </label>
-
         <label>
-        <p style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>Numero de empleado: </p>
-          <input 
-            style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin:'10px' }}
-            type="text" 
-            value={employeeId} 
-            onChange={(e) => setEmployeeId(e.target.value)} 
-            required
-          />
+          <p>Numero de empleado:</p>
+          <input />
         </label>
-
         <button type="submit">Iniciar sesión</button>
-      </form></div>
-    </>
+      </form>
+    </div>
   );
-};
+}
 
-export default LoginForm;
+export default Login;
