ac9efa37caa118f9d1f33bf2a2d1f9634762e737
AlanGtz59 <a1120150064@utch.edu.mx>
2024-03-20 19:53:34 -0600

    se agrego el fondo de imagen del forms y se agrego el tipo de letra usado en este

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Start from './components/start'
+import Start from './components/form'
 export default function App() {
   return (
     <h1 className="text-3xl font-bold underline">
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import './App.css'; // Importa el archivo CSS
+import './form.css'; // Importa el archivo CSS
 
 function Login() {
   return (
+    
     <div className="login-container">
       <form className="formulario">
-        <h1 className="header1">
-        Ingresa tus datos
-        </h1>
+        <h1 className='header1' >Ingresa tus datos</h1>
         <label class="label-t" for="my-input">
-          <p>Rol del empleado:</p>
           <select>
             <option value="">Seleccionar rol</option>
             <option value="Mesero">Mesero</option>
@@ -17,10 +15,8 @@ function Login() {
             <option value="Cajero">Cajero</option>
           </select>
         </label>
-        <label>
-          <p>{'  '}  Numero de empleado:</p>
-          <input />
-        </label>
+        <label for="my-input">Numero de empleado</label>
+        <input id='my-input' type='text'/>
         <button class="my-button" type="submit">Iniciar sesión</button>
       </form>
     </div>
